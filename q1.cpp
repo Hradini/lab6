10ef0f61031f9f20d4b2769b2cdf80c79b993118
Hradini <31266306+Hradini@users.noreply.github.com>
2017-10-22 16:18:30 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a;
+	int *pa;
+	cout << sizeof(pa);
+	cout << sizeof(a);
+	
+	bool b;
+	bool *pb;
+	cout << sizeof(pb);
+	cout << sizeof(b);
+		
+	char c;
+	char *pc;
+	cout << sizeof(pc);
+	cout << sizeof(c);
+}
