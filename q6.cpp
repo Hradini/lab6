f6aebef5df3f185e975208f99a43ca6fc671aedd
Hradini <31266306+Hradini@users.noreply.github.com>
2017-10-24 09:03:48 +0530

    Update q6.cpp

--- q6.cpp
+++ q6.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main(){
 	char str[20], *p;
 	cout << "enter about 10 characters of a string";
 	cin >> str;
 	int i,j;
 	p=str;
-	for(i=0;i<21;++i){
-		for(j=20-i;j<21;++j){
+	int n;
+	for(i=20;i>=0;--i){
+		if(str[i]== '\0'){
+			n=i;
+		}
+	}
+	cout << n;
+	for(i=0;i<n+1;++i){
+		for(j=n-i;j<n+1;++j){
 			cout<<*(p+j);
 		}cout <<endl;
 	}
